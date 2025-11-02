7a95ada16f6e3ba13af89277a7aad36aa6cccf70
Jen Gordon <86987150+Jericgordon@users.noreply.github.com>
2025-11-02 12:12:04 -0700

    Create a very basic map componant that at this point is just an image of the AT map we'd like to use

---
+++ frontend/src/components/Map.jsx
@@ -0,0 +1,11 @@
+import map from "../resources/APPA_Map-1.png";
+
+export default function Map() {
+  console.log("Hello from React!");
+
+  return (
+    <>
+      <img src={map}></img>
+    </>
+  );
+}
